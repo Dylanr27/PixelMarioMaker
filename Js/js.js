d98c0658a3b656bf9555de29c2892a99e612f5b9
Dylanr27 <97147146+Dylanr27@users.noreply.github.com>
2022-03-29 00:58:46 -0700

    Initial commit

---
+++ Js/js.js
@@ -0,0 +1,20 @@
+const colorArray = ["red", "blue", "brown", "yellow", "tan","black", "white"]
+let tiles = document.getElementsByClassName("tile");
+let j = 0;
+let tileIndex = 0;
+for(let i = 0; i < tiles.length; i++) {
+
+    tiles[i].addEventListener('click', function() {
+      //the purpose of this if check is to see if a different tile has been selected
+      if (tileIndex != i){
+        j = 0;
+        tileIndex = i;
+      }
+
+      this.style.backgroundColor = colorArray[j];
+      j++
+      if(j == colorArray.length){
+          j = 0;
+      }
+    });
+}  
